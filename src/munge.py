5c23e2aa572d95fdf23617884e9c0ecf0d9aa199
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2020-10-21 23:55:46 -0400

    fixed data loader

--- src/munge.py
+++ src/munge.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import random
 import torch
+from torch.utils.data import Dataset, DataLoader
 from config import configs
 
 raw_filename = configs['raw_data_path']
@@ -13,7 +14,7 @@ use_full = True  # if True, gives the entire data set. If False, gives us multip
 cat_cols = ['sex', 'age_cat', 'race', 'score_text', 'c_charge_degree']
 date_cols = ['c_jail_in', 'c_jail_out']
 num_cols = ['age', 'decile_score', 'priors_count', 'days_b_screening_arrest', 'is_recid',
-            'two_year_recid'] + date_cols
+            'two_year_recid']
 
 cols = cat_cols + num_cols
 # this may be a config, it will change depending on the csv
@@ -34,13 +35,14 @@ def get_raw_df(filename, use_col_list=True):
 def munge_data(df, filters=None, mutations=None):
     # filter out rows
     if filters:
-        for f in filters: df = df.query(f)
+        for f in filters:
+            df = df.query(f)
 
     # ensure proper dtypes
     for c in cat_cols:
         df[c] = df[c].astype('category')
-    for c in date_cols:
-        df[c] = pd.to_datetime(df[c]).astype(np.int64)  # we want to turn the dates into a single number
+    # for c in date_cols:
+    #     df[c] = pd.to_datetime(df[c]).astype(np.int64)  # we want to turn the dates into a single number
 
     # There are some mutations to add factors but I don't know what they are used for or how to do it in pandas
     # so I think I'll skip it for now and dive deeper later
@@ -48,33 +50,75 @@ def munge_data(df, filters=None, mutations=None):
     return df
 
 
-def create_tensors(df):
+class RecidivismDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data.index)
+
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+        covariate_cols = [x for x in cat_cols + num_cols if x != 'score_text']
+        covariates = row.loc[covariate_cols]
+        score = row.loc['score_text']
+
+        return torch.cat([x.float() for x in covariates.values.tolist()]), score
+
+
+def create_data_loader(df):
+
+    def format_categorical_col(df, col):
+        categories = df[col].cat.categories.tolist()
+        num_categories = len(categories)
+
+        def format_single_cell(x):
+            t = torch.zeros(num_categories)
+            idx = categories.index(x)
+            t[idx] = 1
+            return t
+
+        df[col] = df[col].apply(lambda x: format_single_cell(x))
+
+    def format_numerical_col(df, col):
+        df[col] = df[col].apply(lambda x: torch.tensor([x]))
+
+    for col in cat_cols:
+        format_categorical_col(df, col)
+
+    for col in num_cols:
+        format_numerical_col(df, col)
+
+    dataset = RecidivismDataset(df)
+    return DataLoader(dataset, batch_size=2)
+
+
     # Cat Tensor
-    code_vals = []
-    for c in cat_cols:
-        code_vals.append(df[c].cat.codes.values)
-    cat_data = np.stack(code_vals, 1)
-    cat_tensor = torch.tensor(cat_data, dtype=torch.int64)
-    cat_tensor_ds = torch.utils.data.TensorDataset(cat_tensor)
+    # code_vals = []
+    # for c in cat_cols:
+    #     code_vals.append(df[c].cat.codes.values)
+    # cat_data = np.stack(code_vals, 1)
+    # cat_tensor = torch.tensor(cat_data, dtype=torch.int64)
+    # cat_tensor_ds = torch.utils.data.TensorDataset(cat_tensor)
+    #
+    # # this is a vectorization as opposed to a linearization that might be useful, idk
+    # cat_col_sizes = [len(df[c].cat.categories) for c in cat_cols]
+    # cat_embedding_sizes = [(col_size, min(50, (col_size + 1) // 2)) for col_size in cat_col_sizes]
+    #
+    # # Num Tensor
+    # num_data = np.stack([df[c].values for c in num_cols], 1)
+    # num_tensor = torch.tensor(num_data, dtype=torch.float)
+    # num_tensor_ds = torch.utils.data.TensorDataset(num_tensor)
 
-    # this is a vectorization as opposed to a linearization that might be useful, idk
-    cat_col_sizes = [len(df[c].cat.categories) for c in cat_cols]
-    cat_embedding_sizes = [(col_size, min(50, (col_size + 1) // 2)) for col_size in cat_col_sizes]
-
-    # Num Tensor
-    num_data = np.stack([df[c].values for c in num_cols], 1)
-    num_tensor = torch.tensor(num_data, dtype=torch.float)
-    num_tensor_ds = torch.utils.data.TensorDataset(num_tensor)
-    
     # Not sure if these can be combined, or whether that would make everything confusing?
-    return num_tensor_ds, cat_tensor_ds
+    # return num_tensor_ds, cat_tensor_ds
 
 
-def create_dataloader(df):
-    num_tensor, cat_tensor = create_tensors(df)
-    num_loader = torch.utils.data.DataLoader(num_tensor, batch_size=2, pin_memory=True)
-    cat_loader = torch.utils.data.DataLoader(cat_tensor, batch_size=2, pin_memory=True)
-    return num_loader, cat_loader
+# def create_dataloader(df):
+#     num_tensor, cat_tensor = create_tensors(df)
+#     num_loader = torch.utils.data.DataLoader(num_tensor, batch_size=2, pin_memory=True)
+#     cat_loader = torch.utils.data.DataLoader(cat_tensor, batch_size=2, pin_memory=True)
+#     return num_loader, cat_loader
 
 
 def divide_data_set(filename, size=1000, split=[60, 20, 20]):
@@ -85,25 +129,33 @@ def divide_data_set(filename, size=1000, split=[60, 20, 20]):
 
     # small_data(for other analysis)
     small_df = df.head(size)
-    small_df.to_csv('data/small_data.csv')
+    small_df.to_csv(configs['small_data_path'])
 
     # create dfs for each set(can technically be skipped, but good practice)
     amts = []
     for i in split:
         amts.append(int(n * i / 100))
     train_df = df[:amts[0]]
-    validate_df = df[amts[0]:(amts[0] + amts[1])]
+    validation_df = df[amts[0]:(amts[0] + amts[1])]
     test_df = df[(amts[0] + amts[1]):n]
 
     # create csv's
-    train_df.to_csv('data/training_data.csv')
-    validate_df.to_csv('data/validation_data.csv')
-    test_df.to_csv('data/test_data.csv')
+    train_df.to_csv(configs['train_data_path'])
+    validation_df.to_csv(configs['validation_data_path'])
+    test_df.to_csv(configs['test_data_path'])
 
     # return our dfs
-    return train_df, validate_df, test_df
+    return train_df, validation_df, test_df
+
 
 # Uncomment to get all the data
-# divide_data_set(raw_filename)
+divide_data_set(raw_filename)
 
-#print(create_dataloader(munge_data(get_raw_df(raw_filename))))
+# create train, validation, test data loaders
+train_loader = create_data_loader(munge_data(pd.read_csv(configs['train_data_path'])))
+validation_loader = create_data_loader(munge_data(pd.read_csv(configs['validation_data_path'])))
+test_loader = create_data_loader(munge_data(pd.read_csv(configs['test_data_path'])))
+
+for x, y in train_loader:
+    print(x)
+    print(y)
